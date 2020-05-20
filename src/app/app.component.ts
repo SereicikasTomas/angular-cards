cd26fd0bc1d702bd6ec337ae3a1c7fda5cced311
jacando.tos <sereicikas.t@gmail.com>
2020-05-20 18:20:58 +0200

    add template values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Mountain hiking',
+      imageUrl: 'assets/mountain.jpg',
+      username: '@travelry',
+      content: 'One of the best mountain views.',
+    },
+    {
+      title: 'Magnificent nature',
+      imageUrl: 'assets/trees.jpg',
+      username: '@photogrpher',
+      content: 'Scary, yet beautiful nature.',
+    },
+    {
+      title: 'Adventure biking',
+      imageUrl: 'assets/biking.jpg',
+      username: '@bikerDude',
+      content: 'Bike trail with a view.',
+    },
+  ];
 }
